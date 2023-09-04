e0f820e273b8cb79a68cab96f8690e39489ccd2f
BGUDNA <b@bgudna.com>
2023-09-04 21:15:09 +0200

    getting a basic reponse from server

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('haiii');
+});
+
 app.listen(PORT, () => {
     console.log('I am listening!')
 })

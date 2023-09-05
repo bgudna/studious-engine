d4bee58e44467dbbfc52e5554d5b762827efb431
BGUDNA <b@bgudna.com>
2023-09-05 15:18:17 +0200

    adding basic CRUD functions for database interactions

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,70 @@ app.get('/games', async (request, response) => {
         }
 })
 
+// get a single game from DB
+app.get('/game/:id', async (request, response) => {
+    try {
+        const { gameID } = request.params;
+
+        const game = await Game.findById({gameID})
+        return response.status(200).json({game});
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+// update a already logged game
+app.put('/game/:id', async (request, response) => {
+    try { 
+    if (
+        !request.body.title ||
+        !request.body.publisher ||
+        !request.body.publishYear ||
+        !request.body.genre
+    ) {
+        return response.status(400).send({
+            message: 'Not all required fields have info: title, publisher, publishYear, genre',
+        });
+    }
+
+    const { gameID } = request.params;
+
+    const result = await Game.findByIdAndUpdate(gameID, request.body);
+
+    if(!result) {
+        response.status(404).json( { message: 'Unable to find the selected game' });
+    } else {
+        response.status(200).json( { message: 'Game updated!'});
+    }
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+//delete a game
+app.delete('game/:id', async (request, response) => {
+    try {
+
+        const { gameID } = request.params;
+
+        const result = await Game.findByIdAndDelete(gameID, request.body);
+
+        if(!result) {
+            response.status(404).json( { message: 'Unable to find the selected game' });
+        } else {
+            response.status(200).json( { message: 'Game deleted!'});
+        }
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
 mongoose.connect(mongodbURL)
     .then(() => {
         console.log('Alrighty, we are connected to the DB!');
